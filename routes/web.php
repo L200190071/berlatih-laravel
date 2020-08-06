b22140d86db2554ae494f59d4ea96facc930e81f
shfynfsh21 <shofiya.nafisah2212@gmail.com>
2020-08-06 14:13:48 +0700

    Tugas Template Blade (13) selesai

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,35 @@
 |
 */
 
-Route::get('/', 'HomeController@home');
+/*Route::get('/', 'HomeController@home');
 
 Route::get('/register', 'AuthController@register');
 
 Route::get('/welcome', 'AuthController@welcome');
 
-Route::post('/welcome', 'AuthController@welcome_post');
+Route::post('/welcome', 'AuthController@welcome_post');*/
+
+/*Route::get('/master', function(){
+	return view('master');
+});*/
+
+
+Route::get('/', function () {
+    return view('adminlte.welcome');
+});
+
+Route::get('/data-tables', function () {
+    return view('adminlte.data-tables');
+});
+
+Route::get('/master', function () {
+    return view('adminlte.master');
+});
+
+Route::get('/items', function () {
+    return view('items.index');
+});
+
+Route::get('/items/create', function () {
+    return view('items.create');
+});
